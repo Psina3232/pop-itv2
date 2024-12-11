0b42d97ae7886fb96cb89a04ce6da19469a0ade1
zhura <zhuravlevkos9@gmail.com>
2024-12-11 21:39:13 +0700

    fix bug1

--- app/Middlewares/CSRFMiddleware.php
+++ app/Middlewares/CSRFMiddleware.php
@@ -13,7 +13,6 @@ class CSRFMiddleware
        if ($request->method !== 'POST') {
            return;
        }
-       
        if (empty($request->get('csrf_token')) ||
            $request->get('csrf_token')!==Session::get('csrf_token')) {
            throw new Exception('Request not authorized');
