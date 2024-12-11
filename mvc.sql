313d28248bd39f1d0887904937315cb98b5b05f9
zhura <zhuravlevkos9@gmail.com>
2024-12-12 03:55:54 +0700

    fix db

--- mvc.sql
+++ mvc.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Время создания: Дек 11 2024 г., 16:07
+-- Время создания: Дек 11 2024 г., 21:55
 -- Версия сервера: 10.4.32-MariaDB
 -- Версия PHP: 8.2.12
 
@@ -44,9 +44,7 @@ CREATE TABLE `employees` (
 --
 
 INSERT INTO `employees` (`ID`, `Surname`, `Name`, `Patronym`, `Gender`, `Date_of_Birth`, `Address`, `Age`, `Subunit_ID`) VALUES
-(69, 'Журавлев', 'Константин', 'Александрович', 'man', '2024-12-05', 'Герцена 18', '0', 1),
-(70, 'йцу', 'йцу', 'йцу', 'man', '2024-10-22', 'йцу', '0', 1),
-(72, 'йцу', 'йцу', 'йцу', 'man', '1992-06-18', 'йцуцй', '32', 1);
+(73, 'tyu', 'tyuyt', 'ytuty', 'man', '2024-11-29', 'ytuytuty', '0', 2);
 
 -- --------------------------------------------------------
 
@@ -99,7 +97,9 @@ CREATE TABLE `subunit` (
 INSERT INTO `subunit` (`ID`, `name`, `description`) VALUES
 (1, 'профессорско-преподавательский состав', 'преподаватели'),
 (2, 'учебно-вспомогательный состав', 'учебно-вспомогательный состав'),
-(8, 'административно-хозяйственный состав', 'административно-хозяйственный состав');
+(8, 'административно-хозяйственный состав', 'административно-хозяйственный состав'),
+(16, 'Отдел оценки', ''),
+(17, 'укеук', '');
 
 -- --------------------------------------------------------
 
@@ -120,7 +120,11 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `login`, `password`, `role`) VALUES
 (33, 'admin', '21232f297a57a5a743894a0e4a801fc3', 1),
-(35, 'empl', '200820e3227815ed1756a6b531e7e0d2', 2);
+(35, 'empl', '200820e3227815ed1756a6b531e7e0d2', 2),
+(36, 'zxc', '5fa72358f0b4fb4f2c5d7de8c9a41846', 2),
+(38, 'fff', '343d9040a671c45832ee5381860e2996', 2),
+(39, 'rrr', '44f437ced647ec3f40fa0841041871cd', 2),
+(49, 'zxczxc', '70a5f0c01b9c3e3e01659ca0ca07e4f1', 2);
 
 --
 -- Индексы сохранённых таблиц
@@ -156,6 +160,7 @@ ALTER TABLE `subunit`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `login` (`login`),
   ADD KEY `role` (`role`);
 
 --
@@ -166,7 +171,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблицы `employees`
 --
 ALTER TABLE `employees`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=73;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=74;
 
 --
 -- AUTO_INCREMENT для таблицы `posts`
@@ -184,13 +189,13 @@ ALTER TABLE `role`
 -- AUTO_INCREMENT для таблицы `subunit`
 --
 ALTER TABLE `subunit`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
